88005b932d5b5eab1fd6bcc71ac2090e6a5d143d
G0324D <byesad@gmail.com>
2023-06-18 18:49:42 +0300

    fetch data
    Supabase web sayfasında oluşturduğumuz tablodaki verileri almayı başardık.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,42 @@
 import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
 
 const Home = () => {
-  console.log(supabase)
+
+  const[fetchError, setFetchError] = useState(null)
+  const[supatable, setSupatable]= useState(null)
+
+  useEffect(() =>{
+    const fetchSupatable = async () => {
+      const { data, error} = await supabase
+      .from('supatable')
+      .select()
+
+      if (error) {
+        setFetchError('Veri gelmedi! Hata var.')
+        setSupatable(null)
+        console.log(error)
+      }
+      if(data) {
+        setSupatable(data)
+        setFetchError(null)
+      }
+
+     }
+     fetchSupatable()
+
+  },[])
+  
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {supatable && (
+        <div className="supatable">
+          {supatable.map(supatables =>(
+            <p>{supatables.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
