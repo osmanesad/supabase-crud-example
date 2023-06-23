5e988e3345ad8a364048baeee77885392664e79f
osmanesad <byesad@gmail.com>
2023-06-23 20:27:59 +0300

    Update
    fetchSupatable

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,8 +1,15 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const Update = () => {
 
   const {id} = useParams()
+
+  useEffect(() => {
+    const fetchSupatable = async () => {
+      
+    }
+  })
 
   return (
     <div className="page update">
